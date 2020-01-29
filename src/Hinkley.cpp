b944fbb750e64fbb4e6efcf6e84861e0cdaff2f9
shaylahinkley <hinkleys@mail.gvsu.edu>
2020-01-29 16:26:51 -0500

    Creating the methods

--- src/Hinkley.cpp
+++ src/Hinkley.cpp
@@ -3,4 +3,42 @@
 
 using namespace std;
 
+void MyStack_vector :: push(const int &) {
 
+}
+
+void MyStack_vector:: pop() {
+
+}
+
+int MyStack_vector :: top() const {
+
+}
+
+bool MyStack_vector::empty() const {
+
+}
+
+int MyStack_vector :: GetSize() const {
+
+}
+
+void MyStack_list :: push(const int &) {
+
+}
+
+void MyStack_list :: pop() {
+
+}
+
+int MyStack_list :: top() const {
+
+}
+
+bool MyStack_list :: empty() const {
+
+}
+
+int MyStack_list :: GetSize() const {
+
+}
